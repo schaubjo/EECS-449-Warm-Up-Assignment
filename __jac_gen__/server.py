df36eb304922e997c1bba9a9694213daa71ae382
schaubjo <93454118+schaubjo@users.noreply.github.com>
2024-09-26 13:52:48 -0400

    Add part 1

---
+++ __jac_gen__/server.py
@@ -0,0 +1,40 @@
+from __future__ import annotations
+from jaclang.plugin.feature import JacFeature as _Jac
+from jaclang.plugin.builtin import *
+from dataclasses import dataclass as __jac_dataclass__
+
+@_Jac.make_walker(on_entry=[_Jac.DSFunc('return_message', _Jac.RootType)], on_exit=[])
+@__jac_dataclass__(eq=False)
+class interact(_Jac.Walker):
+
+    def return_message(self, _jac_here_: _Jac.RootType) -> None:
+        _Jac.report({'response': 'Hello, world!'})
+
+@_Jac.make_walker(on_entry=[_Jac.DSFunc('return_message', _Jac.RootType)], on_exit=[])
+@__jac_dataclass__(eq=False)
+class interact_with_body(_Jac.Walker):
+    name: str
+
+    def return_message(self, _jac_here_: _Jac.RootType) -> None:
+        _Jac.report({'response': 'Hello, ' + self.name + '!'})
+
+@_Jac.make_walker(on_entry=[_Jac.DSFunc('return_message', _Jac.RootType)], on_exit=[])
+@__jac_dataclass__(eq=False)
+class multiply(_Jac.Walker):
+    a: int
+    b: int
+
+    def return_message(self, _jac_here_: _Jac.RootType) -> None:
+        res = self.a * self.b
+        _Jac.report({'response': 'Product = ' + str(res)})
+
+@_Jac.make_walker(on_entry=[_Jac.DSFunc('return_message', _Jac.RootType)], on_exit=[])
+@__jac_dataclass__(eq=False)
+class guess_number(_Jac.Walker):
+    guess: int
+
+    def return_message(self, _jac_here_: _Jac.RootType) -> None:
+        if self.guess == 7:
+            _Jac.report({'response': 'You win!'})
+        else:
+            _Jac.report({'response': 'Terrible guess. Try again.'})
